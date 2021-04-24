c716e9979581dec62e6718e673acc60d7547912f
KuroyukiDev <kuroyuki.dev@gmail.com>
2021-04-24 16:27:49 -0700

    Fixed Heroku Support Errors

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-//const express = require('express')
-//const app = express()
-//const port = 3000
-//
-//app.use(express.static("/"))
-//
-//app.get('/', (req, res) => {
-//  res.sendFile('./index.html', { root: __dirname })
-//})
-//
-//app.get('/', (req, res) => {
-//  res.sendFile('./index.html', { root: __dirname })
-//})
-//
-//app.listen(port, () => {
-//  console.log(`Example app listening at http://localhost:${port}`)
-//})
+const express = require('express')
+const app = express()
+const port =  process.env.PORT || 8080
+
+app.use(express.static(__dirname + '/'))
+
+app.get('/download', (req, res) => {
+  res.sendFile('./game_dl.html', { root: __dirname })
+})
+
+app.get('/devlogs', (req, res) => {
+  res.sendFile('./devlog_main.html', { root: __dirname })
+})
+
+app.get('*', (req, res) => {
+  res.sendFile('./index.html', { root: __dirname })
+})
+
+app.listen(port, () => {
+  console.log(`App running on PORT: ${port}`)
+})
